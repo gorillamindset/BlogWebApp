34d7c769becfedaee1b3f7b590e7ba946b020634
abhishek <coderabhishek01@gmail.com>
2024-01-14 01:17:05 +0530

    added Login and signUp Page

---
+++ src/components/Signup.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import authService from "../appwrite/auth";
+import { Button, Input, Logo } from "./index";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+
+function Signup() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        dispatch(login(data));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign up to create account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        <form onSubmit={handleSubmit(create)}>
+          <div className="space-y-5">
+            <Input
+              label="Full name: "
+              placeholder="Enter your full name"
+              {...register("name", { required: true })}
+            />
+
+            <Input
+              label="Email:"
+              placeholder="Enter your email"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPatern: (value) =>
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    "Email address must be a valid address",
+                },
+              })}
+            />
+
+            <Input
+              label="Password: "
+              placeholder="Enter your password"
+              type="password"
+              {...register("password", { required: true })}
+            />
+
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,72 +1,68 @@
-import React, { useEffect } from 'react'
-import { login, logout } from '../../store/authSlice'
-import { useSelector, useDispatch } from 'react-redux'
-import { Logo, Container } from '../index.js'
-import { Link, useNavigate } from 'react-router-dom'
-import LogoutBtn from './LogoutBtn.jsx'
+import React, { useEffect } from "react";
+import { login, logout } from "../../store/authSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { Logo, Container } from "../index.js";
+import { Link, useNavigate } from "react-router-dom";
+import LogoutBtn from "./LogoutBtn.jsx";
 
 function Header() {
-  const authStatus = useSelector((state) => (state.status));
+  const authStatus = useSelector((state) => state.status);
 
-  const navigate = useNavigate()
-  const [loginStatus, setLoginStatus] = useState()
+  const navigate = useNavigate();
+  const [loginStatus, setLoginStatus] = useState();
 
   const navItems = [
     {
-      name: 'Home',
-      slug: '/',
-      active: true
+      name: "Home",
+      slug: "/",
+      active: true,
     },
     {
-      name: 'Login',
-      slug: '/login',
-      active: !authStatus
-    },
-
-    {
-      name: 'Signup',
-      slug: '/signup',
-      active: !authStatus
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
     },
 
     {
-      name: 'All Posts',
-      slug: '/all-posts',
-      active: authStatus
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
     },
 
     {
-      name: 'Add Post',
-      slug: '/add-post',
-      active: authStatus
-    }
-  ]
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
 
   return (
-    <div className='py-3 shadow bg-gray-500 '>
-
+    <div className="py-3 shadow bg-gray-500 ">
       <Container>
-
-        <nav className='flex'>
-          <div className='mr-4'>
-
-            <Link to='/'>
-
-            </Link>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/"></Link>
           </div>
 
-          <ul className='flex ml-auto'>
-
-            {navItems.map((item) => (
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
               item.active ? (
                 <li key={item.name}>
                   <button
                     onClick={() => navigate(item.slug)}
-                    className='inline-block px-6 py-2 duration-200 hover: bg-blue-100 rounded-full'
-                  >{item.name}</button>
+                    className="inline-block px-6 py-2 duration-200 hover: bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
                 </li>
-              ) : (null)
-            ))}
+              ) : null,
+            )}
             {authStatus && (
               <li>
                 <LogoutBtn />
@@ -76,7 +72,7 @@ function Header() {
         </nav>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
